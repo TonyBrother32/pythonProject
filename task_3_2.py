d870f497f66662ca9210e6940a0cb5fb1340fe61
TonyBrother32 <79260951+TonyBrother32@users.noreply.github.com>
2021-02-27 18:24:11 +0300

    Task 3_2

---
+++ task_3_2.py
@@ -0,0 +1,13 @@
+num_words = {"one": "один", "two": "два", "three": "три", "four": "четыре", "five": "пять", "six": "шесть",
+             "seven": "семь", "eight": "восемь", "nine": "девять", "ten": "десять"}
+
+
+def num_translate():
+    num = input("Введите число на английском ")
+    if num[0].isupper():
+        print(f"Перевод на русский '{num_words.get(num.lower()).capitalize()}'")
+    else:
+        print(f"Перевод на русский '{num_words.get(num)}'")
+
+
+num_translate()
