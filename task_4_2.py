10556d5da6b39c7df623208112fa2ec6ed68617a
TonyBrother32 <79260951+TonyBrother32@users.noreply.github.com>
2021-03-03 18:41:58 +0300

    task 4_2

---
+++ task_4_2.py
@@ -0,0 +1,19 @@
+from requests import get, utils
+
+
+def currency_rates(val_item):
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = utils.get_encoding_from_headers(response.headers)
+    content = response.content.decode(encoding=encodings)
+    if 2 < len(val_item) < 4 and int(content.find(val_item.upper())) > 0:
+        index_date = content.find("Date=")
+        date = content[index_date + 6: index_date + 16]
+        index = int(content.find(val_item.upper()))
+        str_value = content[index: index + 100].replace('>', '').replace('</', '').replace(',', '.').split('Value')
+        course = float(str_value[1])
+        print(f'Курс {val_item.upper()} на дату {date} по отношению к рублю равен {course}')
+    else:
+        print("None")
+
+
+currency_rates(input("Введите код валюты "))
