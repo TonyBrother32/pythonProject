82f95353263b583193ba6b7febe75fa13b7edb37
TonyBrother32 <79260951+TonyBrother32@users.noreply.github.com>
2021-03-10 18:11:30 +0300

    task_5_3

--- task_5_3.py
+++ task_5_3.py
@@ -2,6 +2,6 @@ from itertools import zip_longest
 tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Максим', 'Анна']
 classes = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
-res_generator = (el for el in zip_longest(tutors, classes, fillvalue=None))
+res_generator = (el for el in zip_longest(tutors, classes))
 print(*res_generator)
 print(*res_generator, "генератор исчерпал себя")
