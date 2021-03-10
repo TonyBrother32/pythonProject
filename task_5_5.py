dd4d3ab0f35e5820d08d8441bd6e421096075583
TonyBrother32 <79260951+TonyBrother32@users.noreply.github.com>
2021-03-10 18:11:56 +0300

    task_5_5

--- task_5_5.py
+++ task_5_5.py
@@ -1,6 +1,6 @@
 from time import perf_counter
 start = perf_counter()
-src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11, 5, 44, 5, 8]
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 result_src = [num for num in src if src.count(num) == 1]
 print(result_src, perf_counter() - start)
 
