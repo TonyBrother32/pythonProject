6fe8d55bdc295569e887848af74c20e1fbf0043e
TonyBrother32 <79260951+TonyBrother32@users.noreply.github.com>
2021-03-13 16:43:36 +0300

    task_6_1 убрал лишние строки

--- task_6_1.py
+++ task_6_1.py
@@ -1,9 +1,9 @@
-file_1 = open('log.txt', 'r', encoding='utf-8')
 result = []
-for line in file_1:
-    lst_log = line.replace('-', '').replace('"', '').split(' ')
-    lst_request = [lst_log[0], lst_log[5], lst_log[6]]
-    result.append(tuple(lst_request))
-print(result)
-file_1.close()
+with open('log.txt', 'r', encoding='utf-8') as file_1:
+    for line in file_1:
+        lst_log = line.replace('-', '').replace('"', '').split(' ')
+        lst_request = [lst_log[0], lst_log[5], lst_log[6]]
+        result.append(tuple(lst_request))
+    print(result)
+
 
