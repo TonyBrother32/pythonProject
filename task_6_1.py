46cef3312dbf2e1a1abfe7d78a53083368b9babf
TonyBrother32 <79260951+TonyBrother32@users.noreply.github.com>
2021-03-16 20:11:59 +0300

    task_6_1

--- task_6_1.py
+++ task_6_1.py
@@ -4,6 +4,4 @@ with open('log.txt', 'r', encoding='utf-8') as file_1:
         lst_log = line.replace('-', '').replace('"', '').split(' ')
         lst_request = [lst_log[0], lst_log[5], lst_log[6]]
         result.append(tuple(lst_request))
-    print(result)
-
-
+print(result)
