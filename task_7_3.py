77c9963a383a316e00bf53efd65d335304d2ab08
TonyBrother32 <79260951+TonyBrother32@users.noreply.github.com>
2021-03-19 19:32:02 +0300

    task_7_3

---
+++ task_7_3.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+
+shutil.copytree("my_project/authapp/templates", "my_project/templates", dirs_exist_ok=True)
+shutil.copytree("my_project/mainapp/templates", "my_project/templates", dirs_exist_ok=True)
+
+#for root, dirs, files in os.walk("my_project"):
+#    if root.find("templates") > 0 and len(files) == 0:
+#        shutil.copytree(os.path.join(root), "templates", dirs_exist_ok=True)
+
+
